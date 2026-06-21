a279e1e89ee1731124c81731cfeb401fb60ad8f2
tankwars92 <bitbybyte07@gmail.com>
2026-06-21 15:06:18 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import time
-from icq_client import ICQClient, S_ONLINE
+from icq_client import ICQClient, S_ONLINE, XSTATUS_MOBILE
 
 
 SERVER = "kicq.ru"
@@ -13,7 +13,7 @@ c = ICQClient()
 c.icq_server = SERVER
 c.icq_port = PORT
 c.uin, c.password = UIN, PASSWORD
-c.on_login = lambda _: print("Login successful with UIN:", UIN)
+c.on_login = lambda cl: (cl.set_xstatus(XSTATUS_MOBILE, "Test!!!", "Testing XStatus!"), print("Login successful with UIN:", UIN))
 c.on_message_recv = lambda cl, msg, uin: cl.send_message(uin, msg)
 c.on_auth_request = lambda cl, uin, _: cl.send_auth_response(uin, True)
 
